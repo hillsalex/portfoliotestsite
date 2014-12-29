0b4237319807f63a0368492e0989c4cf8b92b0ed
Alex Hills <hills.alex@gmail.com>
2014-12-29 18:22:08 -0500

    Last commit before refactor

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,85 @@
 $(document).ready(function() {
+
+    var startingColorCode = "rgb(146, 151, 66)";
+
+
+
+    var startingHeight = 14;
+    var minHeight = 12;
+    var maxHeight = 18;
+
+    var currentColorCode = startingColorCode;
+
+    var menu = $(".menuHolder");
+    $(".menuHolder").css('width', window.innerWidth + 'px');
+    window.onresize = function() {
+        $(".menuHolder").css('width', window.innerWidth + 'px');
+    }
+
+    $("#colorslider").noUiSlider({
+        start: startingHeight,
+        orientation: "horizontal",
+        range: {
+            min: minHeight,
+            max: maxHeight
+        },
+        step: 1,
+        format: wNumb({
+            decimals: 0
+        })
+    });
+
+
+    $("#colorslider").on('slide', setHeight);
+
+    $(".noUi-handle").attr("data-ui-color-change", "background-color");
+    $(".noUi-handle").addClass("no-before").addClass("no-after");
+
+    $("#menuButton").click(function() {
+        if (menu.css("display") === "none") {
+            menu.show();
+        } else {
+            menu.hide();
+        }
+    });
+
     $(".color-ui-block").each(function(a, b) {
         makeUIClick($(b));
     });
+
+    $("#front").attr("set_bind", "true");
+
+
+    $("#main3d-dev").on('mousewheel', function(event) {
+        var baba = 0;
+        baba++;
+        event.preventDefault();
+    });
+
+
+
+    function setInlineLoadedInterval() {
+        var inline = $("inline");
+        var intervalID = setInterval(function() {
+            if (inline.children().length==0) return;
+
+            clearInterval(intervalID);
+            setColors(currentColorCode);
+
+        }, 10);
+    }
+
+    function setColors(code) {
+        $("[data-ui-color-change]").each(function(x, toChange) {
+            toChange = $(toChange);
+            toChange.css(toChange.attr("data-ui-color-change"), code);
+        });
+        var pieces = splitColorCode(code);
+        changeModelMaterial(Number(pieces[0]) / 255, Number(pieces[1]) / 255, Number(pieces[2]) / 255);
+        $(".noUi-handle").css("background-color", code);
+        $(".noUi-target").css("background-color", code);
+
+    }
 
     function makeUIClick(elt) {
         elt.click(function(a, b) {
@@ -12,24 +90,8 @@ $(document).ready(function() {
             clicked.addClass('selected');
 
             var code = clicked.attr("data-ui-color-code");
-            $("[data-ui-color-change]").each(function(x, toChange) {
-                toChange = $(toChange);
-                toChange.css(toChange.attr("data-ui-color-change"), code);
-            });
-            var pieces = splitColorCode(code);
-            changeModelMaterial(Number(pieces[0])/255,Number(pieces[1])/255,Number(pieces[2])/255);
-            /*
-            tableObj.material.color = new THREE.Color(code);*/
-            runFilterOnImage("bigImageCanvas", bigImage, function(px) {
-
-                return Filters.changeColor(px, code);
-            });
-            runFilterOnImage("smImageCanvas1", smImage1, function(px) {
-                return Filters.changeColor(px, code);
-            });
-            runFilterOnImage("smImageCanvas2", smImage2, function(px) {
-                return Filters.changeColor(px, code);
-            });
+            currentColorCode = code;
+            setColors(code);
         });
     }
 
@@ -59,215 +121,37 @@ $(document).ready(function() {
     });
 
 
-
+    function setHeight() {
+        var setHeight = $("#colorslider").val();
+        var heightRatio = setHeight / startingHeight;
+        var transform = $("#verticalScale");
+        transform.attr("scale", "1 " + heightRatio + " 1");
+        var smImage = $(".smallImage1");
+        var widthRatio = 1 / heightRatio;
+        smImage.css("background-size", (50 * widthRatio) + "% 100%");
+        smImage.css("left", (50 - 50 * widthRatio) + "%");
+    }
 
 
     M_PI = 3.141592;
 
-    Filters = {};
-    Filters.getPixels = function(img) {
-        var c = this.getCanvas(img.width, img.height);
-        var ctx = c.getContext('2d');
-        ctx.drawImage(img, 0, 0);
-        return ctx.getImageData(0, 0, c.width, c.height);
-    };
 
-    Filters.getCanvas = function(w, h) {
-        var c = document.createElement('canvas');
-        c.width = w;
-        c.height = h;
-        return c;
-    };
-    Filters.filterImage = function(filter, image, var_args) {
-        var args = [this.getPixels(image)];
-        for (var i = 2; i < arguments.length; i++) {
-            args.push(arguments[i]);
+    function changeModelMaterial(r, g, b) {
+        var materials = getCurrentModelMaterials();
+        for (var i = 0; i < materials.length; i++) {
+            materials[i].setAttribute("diffuseColor", r + " " + g + " " + b);
         }
-        return filter.apply(null, args);
-    };
-
-    Filters.changeColor = function(pixels, code) {
-        var d = pixels.data;
-        var cs = code.split(',');
-        cs[0] = cs[0].split('(')[1];
-        cs[2] = cs[2].split(')')[0];
-        for (var i = 0; i < d.length; i += 4) {
-            var r = d[i];
-            var g = d[i + 1];
-            var b = d[i + 2];
-            if (r != 255 && g != 255 && b != 255) {
-                d[i] = cs[0];
-                d[i + 1] = cs[1];
-                d[i + 2] = cs[2];
-            } else {
-                d[i + 3] = 0;
-            }
-        }
-        return pixels;
     }
 
-    Filters.tmpCanvas = document.createElement('canvas');
-    Filters.tmpCtx = Filters.tmpCanvas.getContext('2d');
-    Filters.createImageData = function(w, h) {
-        return this.tmpCtx.createImageData(w, h);
-    };
-
-
-    function runFilterOnImage(canvasId, image, filter, arg1, arg2, arg3) {
-        var c = document.getElementById(canvasId);
-        var s = c.previousSibling.style;
-        //var b = c.parentNode.getElementsByTagName('button')[0];
-        //if (b.originalText == null) {
-        //  b.originalText = b.textContent;
-        //}
-        var idata = Filters.filterImage(filter, image, arg1, arg2, arg3);
-        c.width = idata.width;
-        c.height = idata.height;
-        var ctx = c.getContext('2d');
-        ctx.putImageData(idata, 0, 0);
-        //b.textContent = 'Restore original image';
+    function getCurrentModelMaterials() {
+        return $("#main3d-dev").find("material");
     }
 
-    var bigImage = new Image();
-    var smImage1 = new Image();
-    var smImage2 = new Image();
-    bigImage.src = $(".large-dims > img").attr('src');
-    smImage1.src = $(".sm-dims > img:first").attr('src');
-    smImage2.src = $(".sm-dims > img:last").attr('src');
-
-    bigImage.onload = function() {
-        smImage1.onload = function() {
-            smImage2.onload = function() {
-
-                var clicked = $($(".color-ui-block")[0]);
-
-
-                var code = clicked.attr("data-ui-color-code");
-                $("[data-ui-color-change]").each(function(x, toChange) {
-                    toChange = $(toChange);
-                    toChange.css(toChange.attr("data-ui-color-change"), code);
-                });
-
-                runFilterOnImage("bigImageCanvas", bigImage, function(px) {
-                    return Filters.changeColor(px, code);
-                });
-                runFilterOnImage("smImageCanvas1", smImage1, function(px) {
-                    return Filters.changeColor(px, code);
-                });
-                runFilterOnImage("smImageCanvas2", smImage2, function(px) {
-                    return Filters.changeColor(px, code);
-                });
-            };
-        };
-    };
-
-    function initx3d(){
-      var c = $("#canvas3d");
-            var w = c.outerWidth(true);
-            var h = c.outerHeight(true);
-
-    }
-
-    function changeModelMaterial(r,g,b){
-      var materials = getCurrentModelMaterials();
-      for (var i=0;i<materials.length;i++){
-        materials[i].setAttribute("diffuseColor",r + " " + g + " " + b);
-      }
-    }
-
-    function getCurrentModelMaterials(){
-      return $("#main3d").find("material");
-    }
-
-    function splitColorCode(code){
-      var insideParens = code.split("(")[1].split(")")[0];
+    function splitColorCode(code) {
+        var insideParens = code.split("(")[1].split(")")[0];
         return insideParens.split(",");
     }
 
-/*
-    function initthreejs() {
-        var scene, camera, renderer, controls;
-        var geometry, material, mesh;
-
-        init();
-        animate();
-
-        function init() {
-
-            scene = new THREE.Scene();
-            var c = $("#canvas3d");
-            var w = c.outerWidth(true);
-            var h = c.outerHeight(true);
-            camera = new THREE.PerspectiveCamera(75, w / h, 1, 10000);
-            camera.position.z = 50;
-
-
-            controls = new THREE.OrbitControls(camera);
-            controls.damping = 0.2;
-            controls.addEventListener('change', animate);
-
-            geometry = new THREE.BoxGeometry(200, 200, 200);
-            material = new THREE.MeshLambertMaterial({
-                color: new THREE.Color("rgb(146, 151, 66)"),
-                overdraw: 0.5,
-                //side: THREE.DoubleSide,
-            });
-            mesh = new THREE.Mesh(geometry, material);
-            //scene.add(mesh);
-
-            var ambient = new THREE.AmbientLight(0x101030);
-            scene.add(ambient);
-            var directionalLight = new THREE.DirectionalLight(0xffeedd);
-            directionalLight.position.set(0, 100, 100);
-            scene.add(directionalLight);
-
-            var manager = new THREE.LoadingManager();
-            manager.onProgress = function(item, loaded, total) {
-
-            }
-            var loader = new THREE.OBJLoader(manager);
-            loader.load('models/test3.obj', function(object) {
-                object.traverse(function(child) {
-                    if (child instanceof THREE.Mesh) {
-                        child.material = material;
-                        mesh = child;
-                        tableObj = mesh;
-                    }
-                });
-                //object.position.x = 10;
-                object.position.z = -2;
-                object.position.y = -3;
-                object.rotation.x=5.5;
-                scene.add(object);
-            });
-
-
-            //renderer = new THREE.CanvasRenderer({
-            renderer = new THREE.WebGLRenderer({
-                canvas: document.getElementById("canvas3d"),
-                alpha: true
-            });
-
-            renderer.setSize(w, h);
-
-            //document.body.appendChild(renderer.domElement);
-
-        }
-
-        function animate() {
-
-            requestAnimationFrame(animate);
-
-            //mesh.rotation.x += 0.2;
-            //mesh.rotation.y += 0.2;
-
-            renderer.render(scene, camera);
-
-        }
-
-    }
-    initthreejs();*/
-    var tableObj;
+    setColors(startingColorCode);
+    setInlineLoadedInterval();
 });
-
-var viewer;
